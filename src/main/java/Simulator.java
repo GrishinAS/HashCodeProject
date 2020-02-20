cba8052a57f76199d9c7e4a8e59ab192da018fdb
alxndrtarasov <trezor0210@gmail.com>
2020-02-20 22:28:17 +0300

    fkds

--- src/main/java/Simulator.java
+++ src/main/java/Simulator.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Simulator {
-    void simulate(List<Library> libs, int days) {
+    List<Library> simulate(List<Library> libs, int days) {
         Library currentReg = null;
         int daysTillNextReg = 0;
         List<Book> readyBooks = new ArrayList<>();
@@ -13,22 +13,28 @@ public class Simulator {
         List<Library> readyLibs = new ArrayList<>();
         for (int i = 0; i < days; i++) {
             // регаем либы
-            if (currentReg == null) {
+            while (currentReg == null) {
                 if (!libRegOrder.isEmpty()) {
                     currentReg = libRegOrder.remove(0);
-                    daysTillNextReg = currentReg.getSignUpProcess() - 1;
+                    if (currentReg.getSignUpProcess() == 0) {
+                        readyLibs.add(currentReg);
+                        currentReg = null;
+                    } else {
+                        daysTillNextReg = currentReg.getSignUpProcess() - 1;
+                    }
                 }
+            }
+            if (daysTillNextReg == 0) {
+                readyLibs.add(currentReg);
+                currentReg = libRegOrder.remove(0);
+                daysTillNextReg = currentReg.getSignUpProcess() - 1;
             } else {
-                if (daysTillNextReg == 0) {
-                    readyLibs.add(currentReg);
-                    currentReg = libRegOrder.remove(0);
-                } else {
-                    daysTillNextReg--;
-                }
+                daysTillNextReg--;
             }
             readyLibs.forEach(library -> {
                 readyBooks.addAll(library.getMostScoreBooks(readyBooks));
             });
         }
+        return readyLibs;
     }
 }

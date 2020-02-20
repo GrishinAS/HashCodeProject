f09829540ce705761de3eb9ec67a643e1084b1cd
alxndrtarasov <trezor0210@gmail.com>
2020-02-20 21:25:48 +0300

    fkds

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -6,12 +6,20 @@ import java.util.ArrayList;
 
 public class MainApplication {
     ArrayList<Book> allBooks = new ArrayList<>();
+    ArrayList<Library> allLibs;
+    int daysToScan;
     public static void main(String... args) throws Exception {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File("a_example.txt"))))) {
             String line;
+            int lineCounter = 0;
             while ((line = br.readLine()) != null) {
+                if(lineCounter==0) {
+                    String[] firstLine = line.split(" ");
+
+                }
                 new Book();
                 new Library();
+                lineCounter++;
             }
         }
     }

d08e9631d84716e2b646415ff8855064ac69022c
alxndrtarasov <trezor0210@gmail.com>
2020-02-20 22:14:40 +0300

    fkds

--- src/main/java/Simulator.java
+++ src/main/java/Simulator.java
@@ -12,18 +12,23 @@ public class Simulator {
         List<Library> libRegOrder = new ArrayList<>(libs);
         List<Library> readyLibs = new ArrayList<>();
         for (int i = 0; i < days; i++) {
+            // регаем либы
             if (currentReg == null) {
-                currentReg = libRegOrder.remove(0);
-                daysTillNextReg = currentReg.getSignUpProcess() - 1;
+                if (!libRegOrder.isEmpty()) {
+                    currentReg = libRegOrder.remove(0);
+                    daysTillNextReg = currentReg.getSignUpProcess() - 1;
+                }
             } else {
                 if (daysTillNextReg == 0) {
                     readyLibs.add(currentReg);
-                    currentReg = null;
+                    currentReg = libRegOrder.remove(0);
                 } else {
                     daysTillNextReg--;
                 }
             }
-
+            readyLibs.forEach(library -> {
+                readyBooks.addAll(library.getMostScoreBooks(readyBooks));
+            });
         }
     }
 }

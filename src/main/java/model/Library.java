0d2ef5711c71b0830119de3c47232f15700e30bc
GrishinAS <anton.grishin@mts.ru>
2020-02-20 22:12:16 +0300

    Main method addeds

--- src/main/java/model/Library.java
+++ src/main/java/model/Library.java
@@ -1,16 +1,38 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Library {
     private int number;
     private ArrayList<Book> books = new ArrayList<Book>();
+
+    public ArrayList<Book> getAlreadyGivenBooks() {
+        return alreadyGivenBooks;
+    }
+
+    public void setAlreadyGivenBooks(ArrayList<Book> alreadyGivenBooks) {
+        this.alreadyGivenBooks = alreadyGivenBooks;
+    }
+
+    private ArrayList<Book> alreadyGivenBooks = new ArrayList<Book>();
     private int signUpProcess;
     private int booksPerDay;
     static int indexOfLib;
 
     public Library() {
         number = indexOfLib++;
+    }
+
+    List<Book> getMostScoreBooks(List<Book> alreadyScannedBooks){
+        List<Book> resultBooks = new ArrayList<>();
+        for(Book book: this.books){
+            if(!alreadyScannedBooks.contains(book)){
+                resultBooks.add(book);
+            }
+        }
+        alreadyGivenBooks.addAll(resultBooks);
+        return resultBooks;
     }
 
     public int getNumber() {
